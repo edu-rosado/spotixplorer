d4a909646382b3410275275298d57a45ca25ba9d
Eduardo Rosado <e.rosado.fernandez@gmail.com>
2020-11-28 19:58:57 +0100

    Format playlist data (PlaylistRow) and style BasicTable

--- client/src/components/dashboard/dataSection/tables/PlaylistRow.js
+++ client/src/components/dashboard/dataSection/tables/PlaylistRow.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function PlaylistRow({playlist}) {
+export default function PlaylistRow({playlist, cls}) {
 
   const totalSeconds = playlist.tracks
     .reduce((totalSecs,track) => (
       totalSecs + track.duration_ms / 1000
   ),0)
   const hours = Math.floor(totalSeconds / 3600)
-  const minutes = Math.floor(totalSeconds % 3600 / 60)
+  let minutes = Math.floor(totalSeconds % 3600 / 60)
+  minutes = minutes < 10 ? `0${minutes}` : minutes.toString()
   const seconds = Math.round(totalSeconds) % 60
 
   let firstAdd = playlist.tracks[0].added_at
@@ -16,14 +17,16 @@ export default function PlaylistRow({playlist}) {
     if (track.added_at < firstAdd) (firstAdd = track.added_at)
     else if (track.added_at > lastAdd) (lastAdd = track.added_at)
   })
+  firstAdd = `${firstAdd.slice(8,10)}-${firstAdd.slice(5,7)}-${firstAdd.slice(0,4)}`
+  lastAdd = `${lastAdd.slice(8,10)}-${lastAdd.slice(5,7)}-${lastAdd.slice(0,4)}`
 
   return (
-    <tr>
-      <td>{playlist.name}</td>
-      <td>{playlist.tracks.length}</td>
-      <td>{`${hours}h : ${minutes}m : ${seconds}s`}</td>
-      <td>{firstAdd}</td>
-      <td>{lastAdd}</td>
+    <tr className={cls}>
+      <td >{playlist.name}</td>
+      <td className="center">{playlist.tracks.length}</td>
+      <td className="center">{`${hours}:${minutes}:${seconds}`}</td>
+      <td className="center">{firstAdd}</td>
+      <td className="center">{lastAdd}</td>
     </tr>
   )
 }
